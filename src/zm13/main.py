47b3342edb327c56ccc6b26b0500de3eb17e38c5
Shunkm <okuratesu@gmail.com>
2025-06-24 01:59:12 +0900

    new command2

--- src/zm13/main.py
+++ src/zm13/main.py
@@ -30,3 +30,51 @@ def gcd(x: int, y: int):
     """
     typer.echo(mathtools.gcd(x, y))
 
+
+@app.command()
+def lcm(x: int, y: int):
+    """
+    Least Common Multiple
+    """
+    typer.echo(mathtools.lcm(x,y))
+
+
+@app.command()
+def is_prime(x: int):
+    """
+    Prime Number
+    """
+    typer.echo(mathtools.is_prime(x))
+
+
+@app.command()
+def more_study(x: int):
+    """
+    Study
+    """
+    if 0<=x<3:
+        print("何やってるんですか！勉強してください！")
+    elif 3<=x<6:
+        print("もっと燃えられるはずだ。明日も頑張ろう！")
+    elif 6<=x<9:
+        print("なかなかやるじゃないか！")
+    elif 9<=x<13:
+        print("すごいじゃないか！")
+    elif 13<=x<15:
+        print("趣味とかにも，励んだ方がいいぞ。")
+    elif 15<=x<25:
+        print("もう大丈夫だから，体を壊さないようにしてくれよ。")
+    else :
+        pass
+
+@app.command()
+def total_interest(m:int, r:float, n:int):
+    """
+    Compound Interest
+    """
+    typer.echo(m * (1 + r) ** n) 
+
+
+
+
+
